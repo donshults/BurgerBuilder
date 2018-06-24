281fe6a08068bd5d6b577e384ad1313956eb8365
Don Shults <don@tektanium.com>
2018-06-24 07:03:46 -0700

    Menu in Place

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import classes from './SideDrawer.css'
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Aux from '../../../hoc/Aux1';
+import BackDrop from '../../UI/Backdrop/BackDrop';
+
+const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+    return (
+        <Aux>
+            <BackDrop show={props.open}  clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo} >
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    );
+}
+
+export default sideDrawer;
